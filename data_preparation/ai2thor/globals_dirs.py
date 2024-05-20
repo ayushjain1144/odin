11be439184f85b985322da387cb7293c675aa591
ayushjain1144 <ayushjain1144@gmail.com>
2024-05-20 02:32:57 +0000

    ai2thor datagen fix

--- data_preparation/ai2thor/globals_dirs.py
+++ data_preparation/ai2thor/globals_dirs.py
@@ -4,8 +4,8 @@ DATA_DIR = '/projects/katefgroup/language_grounding/SEMSEG_100k_reproduce/ai2tho
 SPLITS_PATH = 'splits/ai2thor_splits'
 
 SPLITS = {
-    'train':   f'{SPLITS_PATH}/scannet_train_512.txt',
-    'val':     f'{SPLITS_PATH}/scannet_val_512.txt',
+    'train':   f'{SPLITS_PATH}/ai2thor_train.txt',
+    'val':     f'{SPLITS_PATH}/ai2thor_val.txt',
     'two_scene': f'{SPLITS_PATH}/two_scene.txt',
     'ten_scene': f'{SPLITS_PATH}/ten_scene.txt'
 }
